34e718cf705d96ded66e0eaa0b38444fe7a26daa
Jigsy <astrojigs24@gmail.com>
2022-06-25 22:24:28 +0100

    BRO
    BRO

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "PulseTracker",
+    name = "pulsewidth",
     version = "1.0.0",
     description="Useful to get coordinates with slopes above a certain threshold value",
-    url='https://github.com/Astrojigs/PulseTracker',
+    url='https://github.com/Astrojigs/pulsewidth',
     license="MIT",
 	packages=find_packages(),
     authors='Jigar and Pratik'
