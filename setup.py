b4bba96e1840f117931bc974636191954eccca9d
Jigsy <astrojigs24@gmail.com>
2022-06-25 22:53:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pulsewidth",
-    version = "1.0.0",
+    version = "1.0.1",
     description="Useful to get coordinates with slopes above a certain threshold value",
     url='https://github.com/Astrojigs/pulsewidth',
     license="MIT",
